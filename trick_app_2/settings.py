74997b22ecb84bd8066ab2d508ad0fac641c152a
James Clawson <nomclawson@byui.edu>
2021-04-22 16:36:13 -0600

    Fixed Security Issue

--- trick_app_2/settings.py
+++ trick_app_2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '818361-gs(_l0cid9+edvg0dctl($1b(@m1br-5txj0%ytr0dq'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
